bbcec3bbfae8fe82fe50c679c0a141d96f911c48
Rusty Toms <jonathantoms5@gmail.com>
2014-01-28 04:13:07 -0500

    format screen names, hashtags, and links in tweets as links

--- rails_app/twitter_challenge_rails/app/assets/javascripts/controllers/user_controller.js
+++ rails_app/twitter_challenge_rails/app/assets/javascripts/controllers/user_controller.js
@@ -1,10 +1,53 @@
 Twitter.UserController = Ember.ObjectController.extend({
-  backgroundPic: function(){
+  backgroundPic: function() {
     return this.get('model.user.profile_banner_url');
   }.property('model.user.profile_banner_url'),
 
-  profilePic: function(){
+  profilePic: function() {
     return this.get('model.user.profile_image_url');
-  }.property('model.user.profile_image_url')
+  }.property('model.user.profile_image_url'),
+
+  numTweets: function() {
+    return this.parseNum(this.get('model.user.statuses_count'));
+  }.property('model.user.statuses_count'),
+
+  numFollowing: function() {
+    return this.parseNum(this.get('model.user.friends_count'));
+  }.property('model.user.friends_count'),
+
+  numFollowers: function() {
+    return this.parseNum(this.get('model.user.followers_count'));
+  }.property('model.user.followers_count'),
+
+  parseNum: function(number) {
+    if (number < 10000) {
+      return number;
+    }
+    if (number < 1000000) {
+      return (Math.floor(number / 100) / 10) + "K";
+    }
+    return (Math.floor(number / 10000) / 100) + "M";
+  }
+});
+
+Twitter.TweetController = Ember.ObjectController.extend({
+  parseTweet: function() {
+    console.log("in parseTweet");
+    var text = this.get("model");
+    var regex = new RegExp("[@#]\\w*", 'g');
+
+    // format all the screen names and hashtags
+    text = text.replace(regex, function(match) {
+      var newStr = "<span class='link-prefix'>";
+      newStr += match[0] + "</span><a>";
+      newStr += match.substring(1, match.length) + "</a>";
+      return newStr;
+    });
+
+    //format all the links
+    regex = new RegExp("(http)s*://\\S*", "gi");
+    text = text.replace(regex, "<a>$&</a>");
+    return text;
+  }.property("model")
 });
 
